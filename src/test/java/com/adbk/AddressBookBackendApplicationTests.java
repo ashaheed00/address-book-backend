ceddbc504c29005ef2bea8d7449cf9aa41867b09
ashaheed00 <ashaheed00@gmail.com>
2020-12-14 22:22:20 +0530

    Creating the project

---
+++ src/test/java/com/adbk/AddressBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
