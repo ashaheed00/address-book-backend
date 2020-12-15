907a02078e390cc11a65732701b883960d000de1
ashaheed00 <ashaheed00@gmail.com>
2020-12-15 09:08:44 +0530

    Added few more proper Validation to DTO class fields, tested manually

--- src/main/java/com/adbk/dto/ContactDTO.java
+++ src/main/java/com/adbk/dto/ContactDTO.java
@@ -1,6 +1,5 @@
 package com.adbk.dto;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import lombok.Data;
@@ -11,17 +10,19 @@ public @Data @NoArgsConstructor class ContactDTO {
 	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}\\s?([A-Z]{1}[a-z]{1,}\\s?){0,2}$", message = "Please enter name in correct format. Eg. John Smith")
 	private String name;
 
-	@NotBlank(message = "Please enter address")
+	@Pattern(regexp = "^\\w[\\w\\s/,-]{3,}$", message = "Please enter address properly")
 	private String address;
-	@NotBlank(message = "Please enter state")
+	@Pattern(regexp = "^[A-Z]{1}[a-z]{3,}$", message = "Please enter state properly")
 	private String state;
-	@NotBlank(message = "Please enter city")
+	@Pattern(regexp = "^[A-Z]{1}[a-z]{3,}$", message = "Please enter city properly")
 	private String city;
 
 	@Pattern(regexp = "^[1-9][0-9]{5}$", message = "Wrong Zip Code format. Eg. 654321")
 	private String zip;
 
+	@Pattern(regexp = "^(\\+?91)?\\s?[0-9]{5}\\s?[0-9]{5}$", message = "Wrong Phone Number Format. Eg. 9009009001, 919009009001, +919009009001, +91 9009009001")
 	private String phoneNo;
 
+	@Pattern(regexp = "^[a-z][0-9a-z_+-]*\\.?[0-9a-z_+-]+@\\w+(\\.[a-z]{2,}){1,2}$", message = "Wrong email format is given. Eg. abc.def@email.co.in")
 	private String email;
 }
